1a3888c2fa94028bdbd196adf8b0bad9b607d86c
Tatsuya-Akashi <tatsuya_akashi@dwango.co.jp>
2020-12-28 16:54:13 +0900

    sharePermissions

--- src/jira.js
+++ src/jira.js
@@ -1975,6 +1975,8 @@ export default class JiraApi {
    * @param {string?} filter.description - A description of the filter.
    * @param {string?} filter.jql - The JQL query for the filter.
    * @param {boolean?} filter.favourite - Whether the filter is selected as a favorite.
+   * @param {string[]?} filter.sharePermissions -
+   *   The groups and projects that the filter is shared with.
    */
   createFilter(filter) {
     return this.doRequest(this.makeRequestHeader(this.makeUri({
@@ -1996,6 +1998,8 @@ export default class JiraApi {
    * @param {string?} filter.description - A description of the filter.
    * @param {string?} filter.jql - The JQL query for the filter.
    * @param {boolean?} filter.favourite - Whether the filter is selected as a favorite.
+   * @param {string[]?} filter.sharePermissions -
+   *   The groups and projects that the filter is shared with.
    */
   updateFilter(filterId, filter) {
     return this.doRequest(this.makeRequestHeader(this.makeUri({
